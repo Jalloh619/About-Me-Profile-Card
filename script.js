6c9e14a14db1e482d71645c2ca0728ff772bf328
Jaleel Bennett <78449846+JaleelB@users.noreply.github.com>
2022-01-06 20:13:22 -0500

    button functionality

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
         function(){$(this).addClass('fa-3x');},
         function(){$(this).removeClass('fa-3x');}
     );
+
+    $('.fa-book').click(() => { $('.profile-card').css("display", "none"); })
+    $('#ba1,#bar2').click(() => { $('.profile-card').css("display", "block"); })
 });
